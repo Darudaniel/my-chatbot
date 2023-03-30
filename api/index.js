39e8f7424b4b90665d4faa64d3d3e7d3cba8f7be
darudaniel <danielelrincon@gmail.com>
2023-03-30 14:16:18 -0500

    cors vercel

--- api/index.js
+++ api/index.js
@@ -10,35 +10,35 @@ const app = express();
 const port = process.env.PORT || 3000;
 app.use(helmet());
 
-// const whitelist = [
-//   'https://notaclinica.com',
-//   'https://darudaniel.github.io',
-//   'http://localhost:3000/',
-// ]
-// const options = {
-//   origin: (origin, callback) => {
-//     if (whitelist.includes(origin)) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error('Dominio no permitido'));
-//     }
-//   }
-// }
-app.use(bodyParser.json());
-// app.use(cors(options))
-// Definir una lista blanca de dominios permitidos
-const whitelist = ['https://darudaniel.github.io/nota-clinica'];
-
-// Opciones de configuración de cors
-const corsOptions = {
-  origin: function(origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
+const whitelist = [
+  'https://notaclinica.com',
+  'https://darudaniel.github.io',
+  'http://localhost:3000/',
+]
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin)) {
+      callback(null, true);
     } else {
-      callback(new Error('Not allowed by CORS'))
+      callback(new Error('Dominio no permitido'));
     }
   }
 }
+app.use(bodyParser.json());
+app.use(cors(options))
+// Definir una lista blanca de dominios permitidos
+// const whitelist = ['https://darudaniel.github.io'];
+
+// // Opciones de configuración de cors
+// const corsOptions = {
+//   origin: function(origin, callback) {
+//     if (whitelist.indexOf(origin) !== -1) {
+//       callback(null, true)
+//     } else {
+//       callback(new Error('Not allowed by CORS'))
+//     }
+//   }
+// }
 
 // Habilitar cors con las opciones de configuración
 app.use(cors(corsOptions));
